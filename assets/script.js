77ae27cb02ca527196e0dc00a70221b1a04b0c10
Sam Knopper <samanthaknopper@hotmail.com>
2022-03-29 08:29:41 +0800

    problem loading button click

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const weatherApiKey = "8b5fae56d0f774f071b372096e2bef5b";
 const clearHistoryButton = document.getElementById('clear-button');
 const searchedHistory = document.getElementById('search-history');
 const searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
-const searchCity = document.getElementById('city-button');
+
 
 //display in a list by city name
 function renderHistory(){
@@ -26,13 +26,15 @@ function renderHistory(){
       searchedHistory.appendChild(button);
     }
 }
+
+
 renderHistory();
 // clear local storage
 clearHistoryButton.addEventListener('click',function(event){
     localStorage.clear();
     window.location.reload();
 })
-
+const searchCity = document.getElementById('city-button');
 
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
@@ -221,16 +223,115 @@ searchForm.addEventListener('submit', function(event){
 
     searchCity.addEventListener('click',function(event){
         event.preventDefault;
+        console.log("button",searchCity);
         let userInput = searchCity.textContent
 
         getWeather(userInput)
-            .then
+        .then(function(weatherData){
+            
+            //data: we need
+
+            //for today
+            const cityName = userInput;
+            const tempToday = weatherData.current.temp;
+            const windToday = weatherData.current.wind_speed;
+            const humToday = weatherData.current.humidity;
+            const uviToday = weatherData.current.uvi;
+
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+
+            today = dd + '/' + mm + '/' + yyyy;
+            document.getElementById('span-date-today').textContent = today;
+           // icon
+            document.getElementById('today-icon').src=iconCodeToPic(weatherData.current.weather[0].icon);
+            document.getElementById('span-city-name').textContent = cityName;
+           // temp 
+           document.getElementById('span-today-temp').textContent = tempToday.toFixed(1);
+           //wind
+           document.getElementById('span-today-wind').textContent = windToday.toFixed(1);
+            //humidity
+           document.getElementById('span-today-humidity').textContent = humToday.toFixed(0);  
+            //uvi
+            document.getElementById('span-today-uvi').textContent = uviToday.toFixed(0);
+
+            var uviColor = document.getElementById("span-today-uvi");
+            
+            if (uviToday < 3) {
+                uviColor.classList.add("green");
+            }
+            else if (uviToday >2 & uviToday < 6) {
+                uviColor.classList.add("yellow");
+            }
+            else if (uviToday >5 & uviToday < 8) {
+                uviColor.classList.add("orange");
+            }
+            else { 
+                uviColor.classList.add("red");
+            }     
+            
+            // 5 day forecast
+            // date
+            //icon
+            //temp
+            //wind
+            //humidity
+            globalThis.forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`;
+            getWeatherText(forecastURL);
+        
+        
+        async function getWeatherText(forecastURL) {
+            let weatherObject = await fetch(forecastURL);
+            let weatherText = await weatherObject.text();
+            parseWeather(weatherText);
+        }
+        
+        let parseWeather = function(weatherText) {
+            let weatherJSON = JSON.parse(weatherText);
+            let dailyForecast = weatherJSON.daily;
+            console.log(dailyForecast);
+            for (x = 0; x < 5; ++x) {
+                let day = dailyForecast[x];
+                let today = new Date().getDay() + x;
+                if (today > 6) {
+                    today = today - 7;
+                }
+                console.log("today is",today);
+                let dayOfWeek = getDayOfWeek(today);
+                let description = day.weather[0].description;
+                let icon = day.weather[0].icon;
+                let temp = day.temp.max;
+                let windSpeed = day.wind_speed;
+                let humidity = day.humidity;
+                displayWeatherDay(dayOfWeek, description, icon, temp, windSpeed, humidity)
+            }
+        }
+        
+        let displayWeatherDay = function(dayOfWeek, description, icon, temp, windSpeed, humidity){
+            let out = "<div class=' forecast-card'><img src='https://openweathermap.org/img/wn/" + icon + "@2x.png'>"
+            out += "<h4>" + dayOfWeek + "</h4>";
+            out += "<p>Temp: " + temp + "°C</p>";
+            out += "<p>Wind Speed: " + windSpeed + "m/s</p>";
+            out += "<p>Humidity: " + humidity + "%</p>";
+            document.getElementById("forecast").innerHTML += out;
+            
+        }
+        
+        let getDayOfWeek = function(dayNum) {
+            var weekday = new Array(7);
+            weekday[0] = "Sunday"
+            weekday[1] = "Monday"
+            weekday[2] = "Tuesday"
+            weekday[3] = "Wednesday"
+            weekday[4] = "Thursday"
+            weekday[5] = "Friday"
+            weekday[6] = "Saturday"
+        
+            return weekday[dayNum];
+        
+        }
 
     })
-  
-
- 
-
-
-
-
+    })
